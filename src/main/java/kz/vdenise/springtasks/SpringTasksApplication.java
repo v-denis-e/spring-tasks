8c0c756b6b6a5e1c1c46f4db9d6e7a591e4fe8a9
valyayev.denis <denis.valyaev@asia-soft.kz>
2021-05-07 14:45:58 +0600

    Исправлены WARN от CheckStyle

--- src/main/java/kz/vdenise/springtasks/SpringTasksApplication.java
+++ src/main/java/kz/vdenise/springtasks/SpringTasksApplication.java
@@ -3,11 +3,12 @@ package kz.vdenise.springtasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс, который запускает Spring Boot приложение.
+ */
 @SpringBootApplication
 public class SpringTasksApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTasksApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTasksApplication.class, args);
+  }
 }
