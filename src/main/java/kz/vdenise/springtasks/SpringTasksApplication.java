2775e95509872b21fe5c9d1e071c2c26c180a609
valyayev.denis <denis.valyaev@asia-soft.kz>
2021-05-07 14:38:16 +0600

    Проинициализирован проект

---
+++ src/main/java/kz/vdenise/springtasks/SpringTasksApplication.java
@@ -0,0 +1,13 @@
+package kz.vdenise.springtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTasksApplication.class, args);
+	}
+
+}
